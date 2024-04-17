55b037c1f041f2c7bdd22e576ba1c7afd6a0cd7c
ChenHaoJie9527 <a18520145493@163.com>
2024-04-17 10:58:56 +0800

    feat: webApp meta info

---
+++ config/index.ts
@@ -0,0 +1,10 @@
+import { AppInfo } from "../types";
+import type { Metadata } from "next";
+
+export const APP_INFO: AppInfo = {
+    title: "mini-defender",
+    description: "this is mini-defender WebApp",
+    default_language: "en",
+    copyright: "",
+    privacy_policy: "",
+}
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+type Locale = "zh" | "en"
+export type AppInfo = {
+    title: string
+    description: string
+    default_language: Locale
+    copyright?: string
+    privacy_policy?: string
+}
