1a3c2a30f993a41f6838b1d81302fac23f7c113c
ChenHaoJie9527 <a18520145493@163.com>
2024-05-04 21:45:50 +0800

    feat: local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,40 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local"
 import { ThemeProvider } from "@/components/theme-provider";
 import ConvexClientProvider from "./ConvexClientProvider";
 import { ModeToggle } from "@/components/modo-toggle";
 import { APP_INFO } from "../../config";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+export const ubuntuFont = localFont({
+  src: [
+    {
+      path: "./fonts/Ubuntu-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Ubuntu-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Ubuntu-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Ubuntu-MediumItalic.ttf",
+      weight: "600",
+    },
+    {
+      path: "./fonts/Ubuntu-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "./fonts/Ubuntu-BoldItalic.ttf",
+      weight: "800",
+    },
+  ],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   ...APP_INFO,
@@ -20,7 +48,7 @@ export default function RootLayout({
   return (
     <ConvexClientProvider>
       <html lang="en" suppressContentEditableWarning>
-        <body className={inter.className}>
+        <body className={ubuntuFont.className}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
