1832f267f044b38d4718fbb50149ba8ac65f5a79
ChenHaoJie9527 <a18520145493@163.com>
2024-05-04 21:27:34 +0800

    feat: route match

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/"]);
+const DEV = process.env.NODE_ENV === 'development';
 
 
 export default clerkMiddleware((auth, req) => {
     if (isProtectedRoute(req)) auth().protect()
-});
+}, {debug: DEV});
 
 
 export const config = {
