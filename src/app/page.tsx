fe53e6985609fbc4ff5fdc8d3415f4cf02645521
ChenHaoJie9527 <a18520145493@163.com>
2024-05-04 22:26:41 +0800

    feat: signOt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
+"use client";
+import {
+  SignInButton,
+  useSession,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const { isLoaded, session, isSignedIn } = useSession();
+
+  if (!isLoaded) {
+    // Add logic to handle loading state
+    return null;
+  }
+  if (!isSignedIn) {
+    // Add logic to handle not signed in state
+    return null;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>hello Next</div>
+    <main className="flex min-h-screen flex-col items-center space-y-4 p-24">
+      <SignedIn>
+        <SignOutButton>
+        <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+
+      <div>
+        <p>
+          This session has been active since{" "}
+          {session.lastActiveAt.toLocaleString()}
+        </p>
+      </div>
     </main>
   );
 }
